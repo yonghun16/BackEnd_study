3a37904773fda1c5666f88ea0df3a302abefec4b
yonghun16 <yonghun16@naver.com>
2022-01-16 00:52:04 +0900

    C

--- TCPL/Chapter_1/exercise_1-08.c
+++ TCPL/Chapter_1/exercise_1-08.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
     int c, nl, space, tab;
 
     nl = 0;
--- TCPL/Chapter_1/exercise_1-09.c
+++ TCPL/Chapter_1/exercise_1-09.c
@@ -1,3 +1,18 @@
 #include <stdio.h>
 
+int main()
+{
+	int	c; 
 
+	while ((c = getchar()) != EOF) {
+		if (c == ' ') {
+			putchar(' ');
+			while ((c = getchar()) != EOF && c == ' ')
+				;
+		}
+		if (c != EOF)
+			putchar(c);
+	}
+
+    return 0;
+}
