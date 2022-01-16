6b9192586c616ec97c50235af25fdb4af4365431
yonghun16 <yonghun16@naver.com>
2022-01-16 14:42:51 +0900

    tcpl

---
+++ TCPL/Chapter_1/exercise_1-20.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+void detab (int n);
+
+int main()
+{
+    detab(4);
+
+    return 0;
+}
+
+void detab (int n)
+{
+    int	column, c;
+
+    column = 0;
+    while((c = getchar()) != EOF)
+        if(c == '\t')
+            do {
+                putchar('@');
+                column++;
+            } while(column % n);
+        else {
+            putchar(c);
+            column = 0;
+        }
+}
