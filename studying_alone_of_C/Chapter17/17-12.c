0bfd9d20fe35cdbb01e4e29a530e16a53d3a6927
yonghun16 <yonghun16@naver.com>
2022-03-13 23:17:30 +0900

    file

--- studying_alone_of_C/Chapter17/17-12.c
+++ studying_alone_of_C/Chapter17/17-12.c
@@ -7,21 +7,36 @@ struct student
     int num;
     double grade;
 };
-typedef struct student Student;
+typedef struct student Student1;
 
-void print_data(Student *ps);
+typedef struct
+{
+    int num;
+    double grade;
+} Student2;
+
+void print_data(Student1 *ps);
+void print_data2(Student2 *ps);
 
 int main(void)
 {
-    Student s1 = { 315, 4.2 }; 
+    Student1 s1 = { 315, 4.2 }; 
+    Student2 s2 = { 215, 3.5 }; 
 
     print_data(&s1);
+    print_data2(&s2);
 
     return 0;
 }
 
-void print_data(Student *ps)
+void print_data(Student1 *ps)
 {
-    printf("학번 : %d\n", ps->num);
-    printf("학점 : %.1lf\n", ps->grade);
+    printf("학생1 학번 : %d\n", ps->num);
+    printf("학생1 학점 : %.1lf\n", ps->grade);
+}
+
+void print_data2(Student2 *ps)
+{
+    printf("학생2 학번 : %d\n", ps->num);
+    printf("학생2 학점 : %.1lf\n", ps->grade);
 }
