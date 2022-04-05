0866b3dc47893589e9dff50277366ffd38482877
yonghun16 <yonghun16@naver.com>
2022-04-05 22:28:20 +0900

    problem

---
+++ studying_alone_of_C/Problems/06-2-3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int i, j;
+
+    for (i = 0; i < 5; i++) {
+        for (j = 0; j < 5; j++) {
+           if(i == j || i+j == 4) {
+               printf("*");
+           }
+           else {
+               printf(" ");
+           }
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
