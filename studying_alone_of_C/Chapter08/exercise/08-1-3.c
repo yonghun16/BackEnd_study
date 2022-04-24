2073e9cb6e6c2f632375c37cbaec26441f775b46
yonghun16 <yonghun16@naver.com>
2022-04-24 20:43:24 +0900

    exercise

---
+++ studying_alone_of_C/Chapter08/exercise/08-1-3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    int A[3] = { 1, 2, 3 };
+    int B[10];
+    int i;
+
+    for (i = 0; i < 10; i++) {
+        B[i] = A[i % 3];
+    }
+
+    for (i = 0; i < 10; i++) {
+        printf("%5d", B[i]);
+    }
+
+    return 0;
+}
