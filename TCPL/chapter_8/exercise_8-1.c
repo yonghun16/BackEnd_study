88189d571ba1c5e6fcb4b83fac40334534f2ac33
yonghun16 <yonghun16@naver.com>
2022-05-14 09:57:35 +0900

    [200~the-unix-system-interface exit

--- TCPL/chapter_8/exercise_8-1.c
+++ TCPL/chapter_8/exercise_8-1.c
@@ -8,43 +8,32 @@
 
 #define BUFSIZE	4096
 
-void	filecopy (int ifd, int ofd);
+void filecopy (int ifd, int ofd);
 
 int main (int argc, char *argv[])
-
 {
-	int	fd;
+    int	fd;
 
-	if (argc == 1)
+    if (argc == 1)
+        filecopy(0, 1);
+    else
+        while (--argc > 0)
+            if ((fd = open(*++argv, O_RDONLY, 0)) == -1) {
+                printf("Error: Cannot open %s!\n", *argv);
+                return 1;
+            } else {
+                filecopy(fd, 1);
+                close(fd);
+            }
 
-		filecopy(0, 1);
-
-	else
-
-		while (--argc > 0)
-
-			if ((fd = open(*++argv, O_RDONLY, 0)) == -1) {
-
-				printf("Error: Cannot open %s!\n", *argv);
-				return 1;
-
-			} else {
-
-				filecopy(fd, 1);
-				close(fd);
-
-			}
-
-	return 0;
+    return 0;
 }
 
 void filecopy (int ifd, int ofd)
-
 {
-	char	buf[BUFSIZE];
-	int	n;
+    char buf[BUFSIZE];
+    int	n;
 
-	while ((n = read(ifd, buf, BUFSIZE)) > 0)
-
-		write(ofd, buf, n);
+    while ((n = read(ifd, buf, BUFSIZE)) > 0)
+        write(ofd, buf, n);
 }
