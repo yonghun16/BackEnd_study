bc1b7d62924ac1b8c4d6ffb7aba7f46fb0a2851b
yonghun16 <yonghun16@naver.com>
2022-05-14 13:34:13 +0900

    hello

--- TCPL/Chapter_1/example_1/2.hello.c
+++ TCPL/Chapter_1/example_1/2.hello.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
-int main() {
-	printf("Hello, World\c");
+int main(void) {
+	printf("Hello, World\c");   // 알려지지 않은 '이스케이프 시퀸스'
+                                // 뒤에 c만 출력됨. 
+
+    return 0;
 }
