1fcb5594c38f1f082f7f15f537e3487ee91ace63
yonghun16 <yonghun16@naver.com>
2022-05-15 10:27:06 +0900

    ch1

--- TCPL/Chapter_1/example_1/26.fahrenheit-celsius-table-function.c
+++ TCPL/Chapter_1/example_1/26.fahrenheit-celsius-table-function.c
@@ -2,25 +2,27 @@
 
 float fahrToCelsius(float fahr);
 
-main()
+int main(void)
 {
-	float fahr, celsius;
-	int lower, upper, step;
-	
-	lower = 0;	/* lower limit of temperature table */
-	upper = 300;	/* upper limit */
-	step = 20;	/* step size */
+    float fahr;
+    int lower, upper, step;
 
-	fahr = lower;
+    lower = 0;      /* lower limit of temperature table */
+    upper = 300;	/* upper limit */
+    step = 20;      /* step size */
 
-	printf("Fahrenheit-Celsius table\n");
-	while (fahr <= upper) {
-		printf("%3.0f %6.1f\n", fahr, fahrToCelsius(fahr));
-		fahr = fahr + step;
-	}
+    fahr = lower;
+
+    printf("Fahrenheit-Celsius table\n");
+    while (fahr <= upper) {
+        printf("%3.0f %6.1f\n", fahr, fahrToCelsius(fahr));
+        fahr = fahr + step;
+    }
+
+    return 0;
 }
 
 float fahrToCelsius(float fahr)
 {
-	return (5.0 / 9.0) * (fahr - 32.0);
+    return (5.0 / 9.0) * (fahr - 32.0);
 }
--- TCPL/Chapter_1/example_1/27.max-line.c
+++ TCPL/Chapter_1/example_1/27.max-line.c
@@ -5,45 +5,45 @@
 int myGetline(char line[], int maxline);
 void copy(char to[], char from[]);
 
-main()
+int main(void)
 {
-	int len;
-	int max;
-	char line[MAXLINE];
-	char longest[MAXLINE];
+    int len;
+    int max;
+    char line[MAXLINE];
+    char longest[MAXLINE];
 
-	max = 0;
-	while ((len = myGetline(line, MAXLINE)) > 0)
-		if (len > max) {
-			max = len;
-			copy(longest, line);
-		}
-	
-	if (max > 0)
-		printf("%s", longest);
-	
-	return 0;
+    max = 0;
+    while ((len = myGetline(line, MAXLINE)) > 0)
+        if (len > max) {
+            max = len;
+            copy(longest, line);
+        }
+
+    if (max > 0)
+        printf("%s", longest);
+
+    return 0;
 }
 
 int myGetline(char s[], int lim)
 {
-	int c, i;
+    int c, i;
 
-	for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
-		s[i] = c;
-	if (c == '\n') {
-		s[i] = c;
-		++i;
-	}
-	s[i] = '\0';
-	return i;
+    for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
+        s[i] = c;
+    if (c == '\n') {
+        s[i] = c;
+        ++i;
+    }
+    s[i] = '\0';
+    return i;
 }
 
 void copy(char to[], char from[])
 {
-	int i;
+    int i;
 
-	i = 0;
-	while ((to[i] = from[i]) != '\0')
-		++i;
+    i = 0;
+    while ((to[i] = from[i]) != '\0')
+        ++i;
 }
