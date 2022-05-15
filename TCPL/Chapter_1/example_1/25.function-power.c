62ae746d24953b9f1141812767c678f75f41a376
yonghun16 <yonghun16@naver.com>
2022-05-15 10:07:30 +0900

    chapter1

--- TCPL/Chapter_1/example_1/25.function-power.c
+++ TCPL/Chapter_1/example_1/25.function-power.c
@@ -2,21 +2,23 @@
 
 int power(int m, int n);
 
-main()
+int main(void)
 {
-	int i;
+    int i;
 
-	for (i = 0; i < 10; ++i)
-		printf("%d %d %d\n", i, power(2, i), power(-3, i));
-	return 0;
+    for (i = 0; i < 10; ++i)
+        printf("%d %d %d\n", i, power(2, i), power(-3, i));
+
+    return 0;
 }
 
 int power(int base, int n)
 {
-	int i, p;
+    int i, p;
 
-	p = 1;
-	for (i = 1; i <= n; ++i)
-		p = p * base;
-	return p;
+    p = 1;
+    for (i = 1; i <= n; ++i)
+        p = p * base;
+
+    return p;
 }
