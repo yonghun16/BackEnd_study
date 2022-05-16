7e406409081eca41053f37dc032ccd2118901444
yonghun16 <yonghun16@naver.com>
2022-05-16 22:40:53 +0900

    TCPL

--- TCPL/Chapter_3/example_3/09.itoa.c
+++ TCPL/Chapter_3/example_3/09.itoa.c
@@ -6,59 +6,61 @@ void itoa(int n, char s[]);
 void reverse(char[]);
 
 
-main()
+int main(void)
 {
-	int a = 0;
-	char s[10];
-	itoa(a, s);
-	printf("%s\n", s);
+    int a = 0;
+    char s[10];
+    itoa(a, s);
+    printf("%s\n", s);
 
-	a = INT_MIN;
-	itoa(a, s);
-	printf("%s\n", s);
+    a = INT_MIN;
+    itoa(a, s);
+    printf("%s\n", s);
 
-	a = INT_MAX;
-	itoa(a, s);
-	printf("%s\n", s);
+    a = INT_MAX;
+    itoa(a, s);
+    printf("%s\n", s);
+
+    return 0;
 }
 
 void itoa(int n, char s[])
 {
-	int i, sign;
-	int preN = n;
-	int nIsMinNagetive = n != 0 && n == -n;
+    int i, sign;
+    int nIsMinNagetive = n != 0 && n == -n;
 
-	if ((sign = n) < 0) {
-		if (nIsMinNagetive) {
-			n += 1;
-			n = -n;
-		} else {
-			n = -n;
-		}
-	}
+    if ((sign = n) < 0) {
+        if (nIsMinNagetive) {
+            n += 1;
+            n = -n;
+        } 
+        else {
+            n = -n;
+        }
+    }
 
-	i = 0;
-	do {
-		s[i++] = n % 10 + '0';
-	} while ((n /= 10) > 0);
+    i = 0;
+    do {
+        s[i++] = n % 10 + '0';
+    } while ((n /= 10) > 0);
 
-	if (sign < 0)
-		s[i++] = '-';
+    if (sign < 0)
+        s[i++] = '-';
 
-	s[i] = '\0';
-	reverse(s);
+    s[i] = '\0';
+    reverse(s);
 
-	if (nIsMinNagetive)
-		s[strlen(s) - 1] += 1;
+    if (nIsMinNagetive)
+        s[strlen(s) - 1] += 1;
 }
 
 void reverse(char s[])
 {
-	int c, i, j;
+    int c, i, j;
 
-	for (i = 0, j = strlen(s) - 1; i < j; i++, j--) {
-		c = s[i];
-		s[i] = s[j];
-		s[j] = c;
-	}
+    for (i = 0, j = strlen(s) - 1; i < j; i++, j--) {
+        c = s[i];
+        s[i] = s[j];
+        s[j] = c;
+    }
 }
