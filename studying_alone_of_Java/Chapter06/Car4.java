ff5fbce54f36f4ee9ee56e9c81935225ed786e08
yonghun16 <yonghun16@naver.com>
2022-06-04 23:32:19 +0900

    java

--- studying_alone_of_Java/Chapter06/Car4.java
+++ studying_alone_of_Java/Chapter06/Car4.java
@@ -18,7 +18,7 @@ public class Car4 {
     void run() {
         for (int i=10; i<=50; i+=10) {
             this.setSpeed(i);
-            System.out.println(this.model + "가 달립니다.(시속:" + this.speed + "km/h)");
+            System.out.println(model + "가 달립니다.(시속:" + this.speed + "km/h)");
         }
     }
 }
