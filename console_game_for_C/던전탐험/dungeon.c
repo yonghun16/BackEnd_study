829c623080500228541386d5485130ed572fb6d3
yonghun16 <yonghun16@naver.com>
2022-07-09 01:04:07 +0900

    던전

---
+++ console_game_for_C/던전탐험/dungeon.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void login();
+void intro();
+
+int main() 
+{
+    intro(); 
+    login();
+
+    return 0;
+}
---
+++ console_game_for_C/던전탐험/login.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void showLoginBar();
+
+void login()
+{
+    showLoginBar();
+    
+    printf("ID : ");
+
+
+
+    showLoginBar();
+
+}
+
+void showLoginBar()
+{
+    printf("\n──────────────────────────────────────────────────────────────────────────────────────────────\n");
+}
