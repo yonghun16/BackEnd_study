f0b573d65888112f4ec8e01432e4f55e35f1d220
yonghun16 <yonghun16@naver.com>
2022-07-12 01:37:44 +0900

    dg

--- console_game_for_C/던전탐험/login.c
+++ console_game_for_C/던전탐험/login.c
@@ -2,14 +2,43 @@
 #include <stdlib.h>
 
 void showLoginBar();
+void showMenu();
+void loginEnter();
+void passwdChangeEnter();
+void gameExit();
 
 void login()
 {
+    char id[10];
+    char menuNumber[10];
+
     showLoginBar();
     
-    printf("ID : ");
+    while(1) {
+        showMenu();
+        scanf("%s", menuNumber); 
 
+        if(menuNumber[1]) {
+            menuNumber[0] = 0;
+        }
 
+        if((int)menuNumber[0]-48 == 1) {        // 로그인
+            loginEnter();
+            break;
+        }
+        else if((int)menuNumber[0]-48 == 2) {   // 패스워드 변경
+            passwdChangeEnter();
+            break;
+        }
+        else if((int)menuNumber[0]-48 == 3) {  // 종료
+            gameExit();
+            break;
+        }
+        else {
+            printf("\n*** 잘못 누르셨습니다. 다시 입력해 주세요. ***\n");
+            continue;
+        }
+    }
 
     showLoginBar();
 
@@ -18,4 +47,27 @@ void login()
 void showLoginBar()
 {
     printf("\n──────────────────────────────────────────────────────────────────────────────────────────────\n");
+}
+
+void showMenu()
+{
+    printf("\n");
+    printf("1. 로그인\n");
+    printf("2. 패스워드 변경\n");
+    printf("3. 종료\n");
+    printf("메뉴 번호를 입력하세요 : ");
+}
+
+void loginEnter() {
+    printf("\n로그인 화면\n");
+}
+
+void passwdChangeEnter()
+{
+    printf("\n패스워드 변경 화면\n");
+}
+
+void gameExit()
+{
+    printf("\n게임 종료\n");
 }
